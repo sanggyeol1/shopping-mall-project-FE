a4cabf7d457671c43e6d321075ff9f926f6c03cc
sanggyeol <sanggyeol1@gmail.com>
2024-06-02 19:59:04 +0900

    local -> proxy

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,7 @@ const PROD_BACKEND = process.env.REACT_APP_PROD_BACKEND;
 const BACKEND_PROXY = process.env.REACT_APP_BACKEND_PROXY;
 
 const api = axios.create({
-  baseURL: LOCAL_BACKEND,
+  baseURL: BACKEND_PROXY,
   headers: {
     "Content-Type": "application/json",
     authorization: `Bearer ${sessionStorage.getItem("token")}`,
