975bb96a96e8dd3ee11d55a5b15a487bb08b842a
sanggyeol <sanggyeol1@gmail.com>
2024-06-06 22:52:34 +0900

    서치기능, 페이지네이션

--- src/page/ProductAll.js
+++ src/page/ProductAll.js
@@ -1,22 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "../component/ProductCard";
 import { Row, Col, Container } from "react-bootstrap";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { productActions } from "../action/productAction";
 import { commonUiActions } from "../action/commonUiAction";
 
 const ProductAll = () => {
+  const navigate = useNavigate()
   const dispatch = useDispatch();
   const error = useSelector((state) => state.product.error);
-  // 처음 로딩하면 상품리스트 불러오기
+  const products = useSelector((state) => state.product.productList);
+  const [query, setQuery] = useSearchParams();
+
+  useEffect(() => {
+    dispatch(productActions.getProductList(Object.fromEntries([...query])));
+  }, [dispatch, query]);
 
   return (
     <Container>
       <Row>
-        <Col md={3} sm={12}>
-          <ProductCard />
-        </Col>
+        {
+          products?.map((product) => (
+            <Col md={3} sm={12}>
+              <ProductCard product={product} />
+            </Col>
+          ))
+        }
       </Row>
     </Container>
   );
